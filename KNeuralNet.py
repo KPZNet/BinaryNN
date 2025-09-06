84965048bb795d36a932c4e5494188420ec7aff4
KennethCeglia <191412790+204028830_bhgithub@users.noreply.github.com>
2025-09-05 20:56:26 -0600

    test c

--- KNeuralNet.py
+++ KNeuralNet.py
@@ -3,6 +3,9 @@ from datetime import *
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# CHANGE
+
 #Print error rate over epochs to see training trend
 def plot_error(epocs, error_history) :
     plt.plot ( epocs, error_history, label="errors" )
